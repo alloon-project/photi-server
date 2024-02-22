0cfc717b666bba83415d6ba89121cd675c0270ca
gs97ahn <gs97ahn@naver.com>
2024-02-23 00:47:52 +0900

    [FEAT] Restdocs 세팅

--- src/test/kotlin/com/alloon/alloonserver/api/develop/DevelopControllerTest.kt
+++ src/test/kotlin/com/alloon/alloonserver/api/develop/DevelopControllerTest.kt
@@ -1,5 +1,42 @@
 package com.alloon.alloonserver.api.develop
 
-import org.junit.jupiter.api.Assertions.*
+import com.alloon.alloonserver.api.RestDocsSupport
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation
+import org.springframework.restdocs.operation.preprocess.Preprocessors
+import org.springframework.restdocs.payload.JsonFieldType
+import org.springframework.restdocs.payload.PayloadDocumentation
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 
-class DevelopControllerTest
+class DevelopControllerTest: RestDocsSupport() {
+
+    @Override
+    override fun initController(): Any {
+        return DevelopController()
+    }
+
+    @DisplayName("헬스 체크를 하면 200을 반환한다")
+    @Test
+    fun givenValid_whenHealthCheck_return200() {
+        // when & then
+        mockMvc.perform(
+            MockMvcRequestBuilders.get("/api/v1/health")
+        ).andDo(MockMvcResultHandlers.print())
+            .andExpect(MockMvcResultMatchers.status().isOk)
+            .andDo(
+                MockMvcRestDocumentation.document(
+                    "health",
+                    Preprocessors.preprocessResponse(Preprocessors.prettyPrint()),
+                    PayloadDocumentation.responseFields(
+                        PayloadDocumentation.fieldWithPath("code").type(JsonFieldType.STRING)
+                            .description("코드"),
+                        PayloadDocumentation.fieldWithPath("message").type(JsonFieldType.STRING)
+                            .description("메세지")
+                    )
+                )
+            )
+    }
+}
