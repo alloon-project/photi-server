26d8d74ae5c3619a8045af5d6d6a30ebdb0c9aea
gs97ahn <gs97ahn@naver.com>
2024-03-09 03:39:15 +0900

    [TEST] DevelopServiceTest 추가

--- src/test/kotlin/com/alloon/alloonserver/api/controller/RestDocsSupport.kt
+++ src/test/kotlin/com/alloon/alloonserver/api/controller/RestDocsSupport.kt
@@ -6,10 +6,12 @@ import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.restdocs.RestDocumentationContextProvider
 import org.springframework.restdocs.RestDocumentationExtension
 import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration
+import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
 import org.springframework.test.web.servlet.setup.StandaloneMockMvcBuilder
 
+@ActiveProfiles("test")
 @ExtendWith(RestDocumentationExtension::class)
 abstract class RestDocsSupport {
 
