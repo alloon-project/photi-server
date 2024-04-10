792a9fe52bab2dbbee88086dc5089dc0838468d5
gs97ahn <gs97ahn@naver.com>
2024-04-10 15:24:42 +0900

    [CHORE] AWS 관련 키 추가 및 jacoco 최소 coverage 60로 설정

---
+++ src/test/kotlin/com/alloon/alloonserver/api/service/s3/S3ServiceTest.kt
@@ -0,0 +1,5 @@
+package com.alloon.alloonserver.api.service.s3
+
+import org.junit.jupiter.api.Assertions.*
+
+class S3ServiceTest
---
+++ src/test/kotlin/com/alloon/alloonserver/common/util/FileUtilityTest.kt
@@ -0,0 +1,5 @@
+package com.alloon.alloonserver.common.util
+
+import org.junit.jupiter.api.Assertions.*
+
+class FileUtilityTest
