5bed8d2bfdcec30644befc9d233e54b030cd649f
koojiny <msj09252@gmail.com>
2024-05-15 11:59:20 +0900

    [FEAT] 미션 이미지 업로드 API - #26

--- src/test/kotlin/com/alloon/alloonserver/api/controller/mission/MissionControllerTest.kt
+++ src/test/kotlin/com/alloon/alloonserver/api/controller/mission/MissionControllerTest.kt
@@ -12,6 +12,8 @@ import org.mockito.Mockito.mock
 import org.mockito.Mockito.`when`
 import org.springframework.http.HttpHeaders.AUTHORIZATION
 import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
+import org.springframework.http.MediaType.MULTIPART_FORM_DATA_VALUE
+import org.springframework.mock.web.MockMultipartFile
 import org.springframework.restdocs.headers.HeaderDocumentation.headerWithName
 import org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders
 import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document
@@ -524,6 +526,44 @@ class MissionControllerTest: RestDocsSupport() {
                     )
                 )
             )
+    }
+
+    @DisplayName("미션 이미지 업로드를 하면 200을 반환한다")
+    @Test
+    fun givenValid_whenUploadMissionImage_thenReturn200() {
+        // given
+        val file = MockMultipartFile("file", "file.png", "image/png", ByteArray(1))
+
+        `when`(missionService.uploadMissionImage(anyLong(), any()))
+                .thenReturn("https://www.google.com")
+
+        // when & then
+        mockMvc.perform(
+                post("/api/missions/image")
+                        .header(AUTHORIZATION, "Bearer access-token")
+                        .principal(mockPrincipal)
+                        .contentType(MULTIPART_FORM_DATA_VALUE)
+                        .param("file", file.toString())
+        ).andDo(print())
+                .andExpect(status().isOk)
+                .andDo(
+                        document(
+                                "mission/upload-image",
+                                preprocessRequest(prettyPrint()),
+                                preprocessResponse(prettyPrint()),
+                                requestHeaders(
+                                        headerWithName(AUTHORIZATION).description("액세스 토큰")
+                                ),
+                                responseFields(
+                                        fieldWithPath("code").type(JsonFieldType.STRING)
+                                                .description("코드"),
+                                        fieldWithPath("message").type(JsonFieldType.STRING)
+                                                .description("메세지"),
+                                        fieldWithPath("data").type(JsonFieldType.STRING)
+                                                .description("데이터")
+                                )
+                        )
+                )
     }
 
     private fun createValidMissionCreateRequest(): MissionCreateRequest {
