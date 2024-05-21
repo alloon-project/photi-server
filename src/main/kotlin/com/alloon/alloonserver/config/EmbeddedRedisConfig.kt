f9e27c6d8ba796d05e71bcbccfb33e435ec19dae
gs97ahn <gs97ahn@naver.com>
2024-05-22 01:35:59 +0900

    [FEAT] Redis Lettuce에서 Redisson으로 변경 - #40

--- src/main/kotlin/com/alloon/alloonserver/config/EmbeddedRedisConfig.kt
+++ src/main/kotlin/com/alloon/alloonserver/config/EmbeddedRedisConfig.kt
@@ -2,8 +2,11 @@ package com.alloon.alloonserver.config
 
 import jakarta.annotation.PostConstruct
 import jakarta.annotation.PreDestroy
-import org.springframework.beans.factory.annotation.Value
+import org.redisson.Redisson
+import org.redisson.api.RedissonClient
+import org.redisson.config.Config
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
 import org.springframework.core.io.ClassPathResource
@@ -23,6 +26,13 @@ class EmbeddedRedisConfig(
         else RedisServer(redisProperties.port)
 
         redisServer.start()
+    }
+
+    @Bean
+    fun redissonClient(): RedissonClient {
+        val config = Config()
+        config.useSingleServer().setAddress("redis://" + redisProperties.host + ":" + redisProperties.port)
+        return Redisson.create(config)
     }
 
     @PreDestroy
