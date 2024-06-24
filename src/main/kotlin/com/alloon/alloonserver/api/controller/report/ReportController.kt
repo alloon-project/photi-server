99cb529c10a84b82c8baf434936a8277c8603c13
YuGyeong98 <kuk3097@naver.com>
2024-06-24 18:12:46 +0900

    [docs] Report 컨트롤러 문서화

--- src/main/kotlin/com/alloon/alloonserver/api/controller/report/ReportController.kt
+++ src/main/kotlin/com/alloon/alloonserver/api/controller/report/ReportController.kt
@@ -2,12 +2,10 @@ package com.alloon.alloonserver.api.controller.report
 
 import com.alloon.alloonserver.api.controller.report.request.ReportCreateRequest
 import com.alloon.alloonserver.api.service.report.ReportService
-import com.alloon.alloonserver.common.constant.SuccessCode.FOUND_REPORT_CATEGORIES
-import com.alloon.alloonserver.common.constant.SuccessCode.REPORT_CREATED
+import com.alloon.alloonserver.common.constant.SuccessCode.*
 import com.alloon.alloonserver.common.response.DefaultListResponse
 import com.alloon.alloonserver.common.response.DefaultResponse
 import com.alloon.alloonserver.common.util.UserUtility
-import com.alloon.alloonserver.domain.report.ReportCategoryType.MISSION
 import jakarta.validation.Valid
 import jakarta.validation.constraints.NotBlank
 import org.springframework.http.ResponseEntity
@@ -21,20 +19,35 @@ class ReportController(
     private val reportService: ReportService,
 ) {
 
+    /**
+     * 200 상태코드와 모든 신고 항목 리스트를 포함한 응답을 반환한다.
+     *
+     * @param reportType 신고 항목
+     * @return [FOUND_REPORT_CATEGORIES] 및 모든 신고 항목 리스트를 포함한 응답
+     */
     @GetMapping("/api/reports/category")
-    fun getAllReportCategories(@RequestParam("type") @NotBlank(message = "신고 항목은 필수 입력입니다.")
-                               reportType: String): ResponseEntity<DefaultListResponse<String>> {
+    fun getAllReportCategories(
+        @RequestParam("type") @NotBlank(message = "신고 항목은 필수 입력입니다.") reportType: String
+    ): ResponseEntity<DefaultListResponse<String>> {
         val response = reportService.getAllReportCategoryDescription(reportType)
 
         return DefaultListResponse.toResponseEntity(FOUND_REPORT_CATEGORIES, response)
     }
 
+    /**
+     * 201 상태코드를 포함한 응답을 반환한다.
+     *
+     * @param principal 사용자 인증 정보
+     * @param request 신고 등록 폼 데이터
+     * @return [REPORT_CREATED]를 포함한 응답
+     */
     @PostMapping("/api/reports")
-    fun reportMission(principal: Principal, @RequestBody @Valid request: ReportCreateRequest):
-            ResponseEntity<DefaultResponse> {
+    fun reportMission(
+        principal: Principal,
+        @RequestBody @Valid request: ReportCreateRequest
+    ): ResponseEntity<DefaultResponse> {
         reportService.createReport(UserUtility.getUserId(principal), request.serviceRequest())
 
         return DefaultResponse.toResponseEntity(REPORT_CREATED)
-
     }
 }
