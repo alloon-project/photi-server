dbce5d9b7abbc9d060dad4ba802b10b515cdfb67
YuGyeong98 <kuk3097@naver.com>
2024-06-24 17:33:56 +0900

    [docs] Mission 컨트롤러 문서화

--- src/main/kotlin/com/alloon/alloonserver/api/controller/mission/MissionController.kt
+++ src/main/kotlin/com/alloon/alloonserver/api/controller/mission/MissionController.kt
@@ -23,6 +23,11 @@ class MissionController(
     private val missionService: MissionService,
 ) {
 
+    /**
+     * 200 상태코드와 모든 챌린지 템플릿 이미지 리스트를 포함한 응답을 반환한다.
+     *
+     * @return [FOUND_MISSION_TEMPLATE_IMAGES] 및 모든 챌린지 템플릿 이미지 리스트를 포함한 응답
+     */
     @GetMapping("/api/missions/image/templates")
     fun getAllMissionTemplateImages(): ResponseEntity<DefaultListResponse<String>> {
         val response = missionService.getAllMissionTemplateImages(LocalDateTime.now())
@@ -30,18 +35,39 @@ class MissionController(
         return DefaultListResponse.toResponseEntity(FOUND_MISSION_TEMPLATE_IMAGES, response)
     }
 
+    /**
+     * 201 상태코드와 생성된 챌린지 정보를 포함한 응답을 반환한다.
+     *
+     * @param principal 사용자 인증 정보
+     * @param request 챌린지 생성 폼 데이터
+     * @return [MISSION_CREATED] 및 생성된 챌린지 정보를 포함한 응답
+     */
     @PostMapping("/api/missions")
-    fun createMission(principal: Principal, @RequestBody @Valid request: MissionCreateRequest):
-            ResponseEntity<DefaultSingleResponse> {
+    fun createMission(
+        principal: Principal,
+        @RequestBody @Valid request: MissionCreateRequest
+    ): ResponseEntity<DefaultSingleResponse> {
         val response = missionService.createMission(UserUtility.getUserId(principal), request.toServiceRequest())
 
         return DefaultSingleResponse.toResponseEntity(MISSION_CREATED, response)
     }
 
-    @PostMapping("/api/missions/image", consumes = [MediaType.MULTIPART_FORM_DATA_VALUE],
-            produces = [MediaType.APPLICATION_JSON_VALUE])
-    fun uploadMissionImage(principal: Principal, @RequestPart(required = false) file: MultipartFile?):
-            ResponseEntity<DefaultSingleResponse> {
+    /**
+     * 200 상태코드와 업로드된 이미지 정보를 포함한 응답을 반환한다.
+     *
+     * @param principal 사용자 인증 정보
+     * @param file 업로드할 이미지 파일 (선택 사항)
+     * @return [MISSION_IMAGE_UPLOADED] 및 업로드된 이미지 정보를 포함한 응답
+     */
+    @PostMapping(
+        "/api/missions/image",
+        consumes = [MediaType.MULTIPART_FORM_DATA_VALUE],
+        produces = [MediaType.APPLICATION_JSON_VALUE]
+    )
+    fun uploadMissionImage(
+        principal: Principal,
+        @RequestPart(required = false) file: MultipartFile?
+    ): ResponseEntity<DefaultSingleResponse> {
         val response = missionService.uploadMissionImage(UserUtility.getUserId(principal), file)
 
         return DefaultSingleResponse.toResponseEntity(MISSION_IMAGE_UPLOADED, response)
