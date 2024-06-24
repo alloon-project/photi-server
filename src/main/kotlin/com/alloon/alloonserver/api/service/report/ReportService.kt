6c2efbe1ecff70beb000f2c2cc51721525b3c768
YuGyeong98 <kuk3097@naver.com>
2024-06-24 18:45:43 +0900

    [docs] Report 서비스 문서화

--- src/main/kotlin/com/alloon/alloonserver/api/service/report/ReportService.kt
+++ src/main/kotlin/com/alloon/alloonserver/api/service/report/ReportService.kt
@@ -1,7 +1,6 @@
 package com.alloon.alloonserver.api.service.report
 
 import com.alloon.alloonserver.api.service.report.request.ReportCreateServiceRequest
-import com.alloon.alloonserver.common.constant.ExceptionCode
 import com.alloon.alloonserver.common.constant.ExceptionCode.*
 import com.alloon.alloonserver.common.constant.RegexPatternConstants.Companion.REPORT_CATEGORY_TYPE_CHARACTER
 import com.alloon.alloonserver.common.response.CustomException
@@ -32,44 +31,50 @@ class ReportService(
 ) {
 
     /**
-     * 신고 항목 설명 전체 조회
-     * @param reportType 신고 항목 종류
-     * @throws REPORT_TYPE_INVALID 400
-     * @return 신고 항목 설명들
+     * 모든 신고 항목 리스트를 반환한다.
+     *
+     * @param reportType 신고 항목
+     * @return 모든 신고 항목 리스트
      */
     fun getAllReportCategoryDescription(
-        @Pattern(regexp = REPORT_CATEGORY_TYPE_CHARACTER,
-            message = "신고 타입은 'MISSION', 'MISSION_MEMBER', 'FEED' 중 하나여야 됩니다.")
-        reportType: String): List<String> {
+        @Pattern(
+            regexp = REPORT_CATEGORY_TYPE_CHARACTER,
+            message = "신고 타입은 'MISSION', 'MISSION_MEMBER', 'FEED' 중 하나여야 됩니다."
+        ) reportType: String
+    ): List<String> {
         return reportCategoryRepository.findAllDescription(ReportCategoryType.valueOf(reportType))
     }
 
     /**
-     * 신고 접수
-     * @param userId 회원 식별자
-     * @param request 신고 생성 요청
-     * @throws USER_NOT_FOUND 404
-     * @throws REPORT_CATEGORY_NOT_FOUND 404
-     * @throws MISSION_NOT_FOUND 404
-     * @throws MISSION_MEMBER_NOT_FOUND 404
-     * @throws FEED_NOT_FOUND 404
+     * 신고를 새로 등록한다.
+     *
+     * @param userId 사용자 id
+     * @param request 유효성 검사가 포함된 신고 등록 폼 데이터
+     * @throws CustomException 사용자 정보를 찾을 수 없을 때 발생한다 ([USER_NOT_FOUND] 404)
+     * @throws CustomException 신고 항목을 찾을 수 없을 때 발생한다 ([REPORT_CATEGORY_NOT_FOUND] 404)
+     * @throws CustomException 챌린지 정보를 찾을 수 없을 때 발생한다 ([MISSION_NOT_FOUND] 404)
+     * @throws CustomException 챌린지 파티원을 찾을 수 없을 때 발생한다 ([MISSION_MEMBER_NOT_FOUND] 404)
+     * @throws CustomException 챌린지 피드를 찾을 수 없을 때 발생한다 ([FEED_NOT_FOUND] 404)
      */
     fun createReport(userId: Long, @Valid request: ReportCreateServiceRequest) {
         val reporter = userRepository.find(userId) ?: throw CustomException(USER_NOT_FOUND)
 
-        val reportCategory = reportCategoryRepository.find(request.reportCategoryId,
-            ReportCategoryType.valueOf(request.reportType)) ?: throw CustomException(REPORT_CATEGORY_NOT_FOUND)
+        val reportCategory = reportCategoryRepository.find(
+            request.reportCategoryId, ReportCategoryType.valueOf(request.reportType)
+        ) ?: throw CustomException(REPORT_CATEGORY_NOT_FOUND)
 
         when (ReportCategoryType.valueOf(request.reportType)) {
             MISSION -> {
                 val mission = missionRepository.find(request.reportTargetId) ?: throw CustomException(MISSION_NOT_FOUND)
                 reportRepository.save(request.toEntity(reportCategory, reporter, mission = mission))
             }
+
             MISSION_MEMBER -> {
                 val missionMember = missionMemberRepository.find(request.reportTargetId)
                     ?: throw CustomException(MISSION_MEMBER_NOT_FOUND)
                 reportRepository.save(request.toEntity(reportCategory, reporter, missionMember = missionMember))
             }
+
             FEED -> {
                 val feed = feedRepository.find(request.reportTargetId) ?: throw CustomException(FEED_NOT_FOUND)
                 reportRepository.save(request.toEntity(reportCategory, reporter, feed = feed))
