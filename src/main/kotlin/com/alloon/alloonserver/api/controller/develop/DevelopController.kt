40a7aaea1ef234a3c4b2c224bfc52572f91e16b1
YuGyeong98 <kuk3097@naver.com>
2024-06-28 15:34:44 +0900

    [docs] 서버, 앱 버전 api 에러코드 응답 추가

--- src/main/kotlin/com/alloon/alloonserver/api/controller/develop/DevelopController.kt
+++ src/main/kotlin/com/alloon/alloonserver/api/controller/develop/DevelopController.kt
@@ -1,15 +1,12 @@
 package com.alloon.alloonserver.api.controller.develop
 
 import com.alloon.alloonserver.api.service.develop.DevelopService
-import com.alloon.alloonserver.api.service.develop.response.DevelopIsNeedForceUpdateResponse
 import com.alloon.alloonserver.common.constant.SuccessCode.FORCE_UPDATE
 import com.alloon.alloonserver.common.constant.SuccessCode.SERVER_OK
 import com.alloon.alloonserver.common.response.DefaultResponse
 import com.alloon.alloonserver.common.response.DefaultSingleResponse
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.Parameter
-import io.swagger.v3.oas.annotations.media.Content
-import io.swagger.v3.oas.annotations.media.Schema
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
 import io.swagger.v3.oas.annotations.tags.Tag
@@ -41,11 +38,8 @@ class DevelopController(
     @Operation(summary = "앱 강제 업데이트 필요 여부 조회", description = "사용자의 앱 버전을 확인하고, 앱 업데이트가 필요한지 조회합니다.")
     @ApiResponses(
         value = [
-            ApiResponse(
-                responseCode = "200",
-                description = "앱 강제 업데이트 필요 여부 조회 성공",
-                content = [Content(schema = Schema(implementation = DevelopIsNeedForceUpdateResponse::class))]
-            )
+            ApiResponse(responseCode = "200", description = "앱 강제 업데이트 필요 여부 조회 성공"),
+            ApiResponse(responseCode = "400", description = "버전은 필수 입력입니다."),
         ]
     )
     fun needForceUpdate(
