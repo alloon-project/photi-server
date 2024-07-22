148372687717fd6147c6febf6ff621801b057e08
YuGyeong98 <kuk3097@naver.com>
2024-07-22 17:45:19 +0900

    [refactor] 챌린지 생성 dto 수정

---
+++ src/main/kotlin/com/alloon/alloonserver/service/mission/dto/CreateMissionDto.kt
@@ -0,0 +1,16 @@
+package com.alloon.alloonserver.service.mission.dto
+
+import java.time.LocalDate
+import java.time.LocalTime
+
+data class CreateMissionDto(
+    val name: String,
+    val isPublic: Boolean,
+    val goal: String,
+    val proveTime: LocalTime,
+    val endDate: LocalDate,
+    val imageUrl: String,
+    val rules: List<CreateMissionRuleDto>,
+//    val hashtags: List<CreateMissionHashtagDto>,
+) {
+}
