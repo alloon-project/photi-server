7964bec83410ae11b2c0f2dc5e5448739886861d
YuGyeong98 <kuk3097@naver.com>
2024-07-23 15:57:26 +0900

    [test] 스키마 변경으로 인한 테스트 fail 수정

--- src/test/kotlin/com/alloon/alloonserver/domain/feed/FeedRepositoryTest.kt
+++ src/test/kotlin/com/alloon/alloonserver/domain/feed/FeedRepositoryTest.kt
@@ -10,6 +10,9 @@ import com.alloon.alloonserver.domain.user.ContactRepository
 import com.alloon.alloonserver.domain.user.User
 import com.alloon.alloonserver.domain.user.UserRepository
 import com.alloon.alloonserver.framework.TestContainerInitializer
+import com.alloon.alloonserver.service.mission.dto.CreateMissionDto
+import com.alloon.alloonserver.service.mission.dto.CreateMissionHashtagDto
+import com.alloon.alloonserver.service.mission.dto.CreateMissionRuleDto
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
@@ -19,6 +22,7 @@ import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.transaction.annotation.Transactional
 import java.time.LocalDate
+import java.time.LocalTime
 
 @ActiveProfiles("test")
 @SpringBootTest
@@ -46,17 +50,8 @@ class FeedRepositoryTest(
         assertThat(result).isEqualTo(feed)
     }
 
-
     private fun createAdnSaveFeed(): Feed {
-        val mission = missionRepository.save(
-            Mission(
-                missionName = "미션명", description = "미션 설명", goal = "미션 목표",
-                imageUrl = "https://alloon.s3.us-east-2.amazonaws.com/alloon-logo.png",
-                endDate = LocalDate.of(2999, 1, 1),
-                hashtags = listOf("러닝")
-            )
-        )
-
+        val mission = createAndSaveMission()
         val contact = contactRepository.save(
             Contact(
                 email = "tester@alloon.com",
@@ -74,8 +69,34 @@ class FeedRepositoryTest(
             )
         )
         val missionMember =
-            missionMemberRepository.save(MissionMember(user = user, mission = mission, creatorYn = true))
+            missionMemberRepository.save(MissionMember(user = user, mission = mission, isCreator = true))
 
         return feedRepository.save(Feed(missionMember = missionMember, mission = mission, imageUrl = ""))
+    }
+
+    private fun createAndSaveMission(): Mission {
+        val dto = getCreateMissionDto()
+        val mission = Mission.toEntity(dto)
+        return missionRepository.save(mission)
+    }
+
+    private fun getCreateMissionDto(): CreateMissionDto {
+        return CreateMissionDto(
+            "챌린지 이름",
+            true,
+            "챌린지 목표입니다.",
+            LocalTime.of(13, 0),
+            LocalDate.of(2024, 12, 1),
+            "https://url.kr/5MhHhD",
+            listOf(
+                CreateMissionRuleDto("챌린지 인증 룰1"),
+                CreateMissionRuleDto("챌린지 인증 룰2"),
+                CreateMissionRuleDto("챌린지 인증 룰3"),
+            ),
+            listOf(
+                CreateMissionHashtagDto("해시태그 1"),
+                CreateMissionHashtagDto("해시태그 2"),
+            )
+        )
     }
 }
--- src/test/kotlin/com/alloon/alloonserver/domain/report/ReportRepositoryTest.kt
+++ src/test/kotlin/com/alloon/alloonserver/domain/report/ReportRepositoryTest.kt
@@ -10,8 +10,10 @@ import com.alloon.alloonserver.domain.user.Contact
 import com.alloon.alloonserver.domain.user.ContactRepository
 import com.alloon.alloonserver.domain.user.User
 import com.alloon.alloonserver.domain.user.UserRepository
-import com.alloon.alloonserver.framework.TestContainerConfig
 import com.alloon.alloonserver.framework.TestContainerInitializer
+import com.alloon.alloonserver.service.mission.dto.CreateMissionDto
+import com.alloon.alloonserver.service.mission.dto.CreateMissionHashtagDto
+import com.alloon.alloonserver.service.mission.dto.CreateMissionRuleDto
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
@@ -21,6 +23,7 @@ import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.transaction.annotation.Transactional
 import java.time.LocalDate
+import java.time.LocalTime
 
 @ActiveProfiles("test")
 @SpringBootTest
@@ -73,13 +76,27 @@ class ReportRepositoryTest(
     }
 
     private fun createAndSaveMission(): Mission {
-        return missionRepository.save(
-            Mission(
-                missionName = "미션명",
-                description = "미션 설명",
-                goal = "미션 목표",
-                imageUrl = "https://alloon.s3.us-east-2.amazonaws.com/alloon-logo.png",
-                endDate = LocalDate.of(2999, 1, 1)
+        val dto = getCreateMissionDto()
+        val mission = Mission.toEntity(dto)
+        return missionRepository.save(mission)
+    }
+
+    private fun getCreateMissionDto(): CreateMissionDto {
+        return CreateMissionDto(
+            "챌린지 이름",
+            true,
+            "챌린지 목표입니다.",
+            LocalTime.of(13, 0),
+            LocalDate.of(2024, 12, 1),
+            "https://url.kr/5MhHhD",
+            listOf(
+                CreateMissionRuleDto("챌린지 인증 룰1"),
+                CreateMissionRuleDto("챌린지 인증 룰2"),
+                CreateMissionRuleDto("챌린지 인증 룰3"),
+            ),
+            listOf(
+                CreateMissionHashtagDto("해시태그 1"),
+                CreateMissionHashtagDto("해시태그 2"),
             )
         )
     }
