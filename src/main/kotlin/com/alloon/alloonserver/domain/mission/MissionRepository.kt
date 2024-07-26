085ccc596e255700b2fdd1ac0c3cd53bdb505809
YuGyeong98 <kuk3097@naver.com>
2024-07-26 13:44:00 +0900

    [feature] 활성화된 챌린지만 조회 레포지토리 메소드 구현

--- src/main/kotlin/com/alloon/alloonserver/domain/mission/MissionRepository.kt
+++ src/main/kotlin/com/alloon/alloonserver/domain/mission/MissionRepository.kt
@@ -1,7 +1,11 @@
 package com.alloon.alloonserver.domain.mission
 
+import com.alloon.alloonserver.domain.base.ServiceStatus
 import com.alloon.alloonserver.domain.mission.custom.MissionCustomRepository
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface MissionRepository : JpaRepository<Mission, Long>, MissionCustomRepository {
+
+    fun findPopularByServiceStatus(status: ServiceStatus, pageable: Pageable): List<Mission>
 }
