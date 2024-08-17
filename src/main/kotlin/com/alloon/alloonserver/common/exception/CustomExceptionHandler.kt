b8b484eddb21410f64eba9141c43407cca4233d5
YuGyeong98 <kuk3097@naver.com>
2024-08-17 16:37:42 +0900

    [refactor] handleConstraintViolationException 메소드 수정

--- src/main/kotlin/com/alloon/alloonserver/common/exception/CustomExceptionHandler.kt
+++ src/main/kotlin/com/alloon/alloonserver/common/exception/CustomExceptionHandler.kt
@@ -6,14 +6,13 @@ import com.alloon.alloonserver.common.response.ErrorResponse
 import com.alloon.alloonserver.common.response.ExceptionResponse
 import com.fasterxml.jackson.databind.exc.MismatchedInputException
 import io.sentry.Sentry
+import jakarta.servlet.http.HttpServletRequest
 import jakarta.validation.ConstraintViolationException
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus.BAD_REQUEST
 import org.springframework.http.HttpStatusCode
 import org.springframework.http.ResponseEntity
 import org.springframework.http.converter.HttpMessageNotReadableException
-import org.springframework.security.access.AccessDeniedException
-import org.springframework.web.HttpRequestMethodNotSupportedException
 import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.ServletRequestBindingException
 import org.springframework.web.bind.annotation.ExceptionHandler
@@ -84,55 +83,21 @@ class CustomExceptionHandler : ResponseEntityExceptionHandler() {
     }
 
     @ExceptionHandler(ConstraintViolationException::class)
-    fun handleConstraintViolationException(ex: ConstraintViolationException):
-            ResponseEntity<ExceptionResponse> {
-        var responseCode: String
-        val responseMessage: String
-
-        if (ex.constraintViolations.stream().findFirst().isPresent) {
-            val constraintViolation = ex.constraintViolations.stream()
-                .findFirst()
-                .get()
-            val propertyPathSize = constraintViolation.propertyPath.toString()
-                .split("\\.")
-                .size
-
-            responseCode = constraintViolation.propertyPath.toString()
-                .split("\\.")[propertyPathSize - 1]
-            responseCode = responseCode.replace("[^\\p{Alnum}]+", "_")
-                .replace("(\\p{Lower})(\\p{Upper})", "$1_$2")
-                .uppercase()
-            responseCode = formatIfInnerDto(responseCode)
-            responseMessage = constraintViolation.messageTemplate
-            responseCode += formatResponseCode(responseMessage)
-            responseCode = responseCode.split(".")[responseCode.split(".").size - 1]
-        } else {
-            throw CustomException(SERVER_ERROR)
+    fun handleConstraintViolationException(
+        ex: ConstraintViolationException,
+        request: HttpServletRequest
+    ): ResponseEntity<ErrorResponse> {
+        val message = mutableListOf<Map<String?, String>>()
+        ex.constraintViolations.forEach {
+            val propertyPaths = it.propertyPath.toString().split(".")
+            val property = propertyPaths.lastOrNull()
+            message.add(mapOf(property to it.message))
         }
+        val path = request.requestURL.toString()
 
-        return ResponseEntity.status(BAD_REQUEST)
-            .body(ExceptionResponse(responseCode, responseMessage))
-    }
+        val response = ErrorResponse(BAD_REQUEST.value(), BAD_REQUEST.name, message, path)
 
-    /**
-     * 401 Unauthorized
-     */
-    @ExceptionHandler(AccessDeniedException::class)
-    protected fun handleAccessDeniedException(ex: AccessDeniedException): ResponseEntity<ExceptionResponse> {
-        return ExceptionResponse.toResponseEntity(TOKEN_UNAUTHENTICATED)
-    }
-
-    /**
-     * 405 Method Not Allowed
-     */
-    override fun handleHttpRequestMethodNotSupported(
-        ex: HttpRequestMethodNotSupportedException,
-        headers: HttpHeaders,
-        status: HttpStatusCode,
-        request: WebRequest
-    ): ResponseEntity<Any>? {
-        return ResponseEntity.status(METHOD_DISABLED.httpStatus)
-            .body(ExceptionResponse(METHOD_DISABLED))
+        return ResponseEntity.status(BAD_REQUEST).body(response)
     }
 
     /**
@@ -164,45 +129,5 @@ class CustomExceptionHandler : ResponseEntityExceptionHandler() {
     protected fun handleInterruptedException(ex: InterruptedException): ResponseEntity<ExceptionResponse> {
         Sentry.captureException(ex)
         return ExceptionResponse.toResponseEntity(SERVER_ERROR)
-    }
-
-    private fun formatIfInnerDto(responseCode: String): String {
-        if (!responseCode.matches(".*\\d+.*".toRegex()))
-            return responseCode
-
-        for (i in responseCode.length - 1 downTo 0) {
-            val c = responseCode[i]
-            if (c.isDigit())
-                return responseCode.substring(i + 2)
-        }
-
-        return responseCode
-    }
-
-    private fun formatResponseCode(responseMessage: String): String {
-        var responseCode = ""
-
-        if (responseMessage.contains("필수 입력입니다.") || responseMessage.contains("첨부해 주세요")) {
-            responseCode = "_FIELD_REQUIRED" // @NotBlank, @NotNull
-        } else if (responseMessage.contains("~")) {
-            responseCode = "_LENGTH_INVALID" // @Size
-        } else if (responseMessage.contains("형식") || responseMessage.contains("조합")) {
-            responseCode = "_FORMAT_INVALID" // @Pattern, @Email - format
-        } else if (responseMessage.contains("중 하나여야 됩니다.")) {
-            if (responseCode.contains("TYPE")) responseCode.removeSurrounding("TYPE")
-            responseCode = "_TYPE_INVALID" // @Pattern - type
-        } else if (responseMessage.contains("까지의 수만")) {
-            responseCode = "_RANGE_INVALID" // @Range
-        } else if (responseMessage.contains("0 또는 양수")) {
-            responseCode = "_POSITIVE_OR_ZERO_ONLY" // @PositiveOrZero
-        } else if (responseMessage.contains("양수")) {
-            responseCode = "_POSITIVE_ONLY" // @Positive
-        } else if (responseMessage.contains("요청")) {
-            responseCode = "REQUEST_INVALID"
-        } else if (responseMessage.contains("앞설 수 없습니다.")) {
-            responseCode = "_FUTURE_INVALID" // @Future
-        }
-
-        return responseCode
     }
 }
