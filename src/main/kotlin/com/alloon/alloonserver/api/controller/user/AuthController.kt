bc951c75ca145b58147553fb3990caeb6595bcf4
YuGyeong98 <kuk3097@naver.com>
2024-09-03 21:54:08 +0900

    #78 [refactor] 인증 API 어노테이션 적용

--- src/main/kotlin/com/alloon/alloonserver/api/controller/user/AuthController.kt
+++ src/main/kotlin/com/alloon/alloonserver/api/controller/user/AuthController.kt
@@ -1,7 +1,9 @@
 package com.alloon.alloonserver.api.controller.user
 
 import com.alloon.alloonserver.api.controller.user.request.*
-import com.alloon.alloonserver.common.constant.RegexPatternConstants
+import com.alloon.alloonserver.common.constant.ExceptionCode.*
+import com.alloon.alloonserver.common.constant.RegexPatternConstants.Companion.LOWERCASE_NUMBER_UNDERSCORE
+import com.alloon.alloonserver.common.response.ApiErrorResponses
 import com.alloon.alloonserver.common.response.StringResponse
 import com.alloon.alloonserver.common.util.UserUtility
 import com.alloon.alloonserver.config.SwaggerConfig.Companion.ACCESS_TOKEN_KEY
@@ -14,7 +16,6 @@ import com.alloon.alloonserver.service.user.response.UserRegisterResponse
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.Parameter
 import io.swagger.v3.oas.annotations.responses.ApiResponse
-import io.swagger.v3.oas.annotations.responses.ApiResponses
 import io.swagger.v3.oas.annotations.security.SecurityRequirement
 import io.swagger.v3.oas.annotations.tags.Tag
 import jakarta.validation.Valid
@@ -38,12 +39,8 @@ class AuthController(
 
     @PostMapping("/api/contacts")
     @Operation(summary = "이메일 인증코드 전송")
-    @ApiResponses(
-        value = [
-            ApiResponse(responseCode = "201", description = "이메일 인증코드 전송 성공"),
-            ApiResponse(responseCode = "409", description = "이미 사용중인 이메일입니다."),
-        ]
-    )
+    @ApiResponse(responseCode = "201")
+    @ApiErrorResponses([EXISTING_EMAIL])
     fun sendVerificationCode(@RequestBody @Valid request: ContactSendVerificationRequest): ResponseEntity<StringResponse> {
         authService.sendVerificationCode(request.toServiceDto())
 
@@ -53,13 +50,8 @@ class AuthController(
 
     @PatchMapping("/api/contacts/verify")
     @Operation(summary = "이메일 인증코드 확인")
-    @ApiResponses(
-        value = [
-            ApiResponse(responseCode = "200", description = "이메일 인증코드 확인 성공"),
-            ApiResponse(responseCode = "400", description = "이메일 인증코드가 틀렸습니다."),
-            ApiResponse(responseCode = "404", description = "존재하지 않는 이메일입니다."),
-        ]
-    )
+    @ApiResponse(responseCode = "200")
+    @ApiErrorResponses([EMAIL_VERIFICATION_CODE_INVALID, EMAIL_NOT_FOUND])
     fun verifyEmailVerificationCode(@RequestBody @Valid request: ContactVerifyRequest): ResponseEntity<StringResponse> {
         authService.verifyEmailVerificationCode(request.toServiceDto())
 
@@ -68,25 +60,11 @@ class AuthController(
 
     @GetMapping("/api/users/username")
     @Operation(summary = "아이디 검증")
-    @ApiResponses(
-        value = [
-            ApiResponse(responseCode = "200", description = "아이디 검증 성공"),
-            ApiResponse(
-                responseCode = "409",
-                description = """
-                1. 사용 불가능한 아이디입니다.
-                2. 이미 사용중인 아이디입니다.
-                """
-            ),
-        ]
-    )
+    @ApiResponse(responseCode = "200")
+    @ApiErrorResponses([USERNAME_FORMAT_INVALID, UNAVAILABLE_USERNAME, EXISTING_USERNAME])
     fun validateUsername(
-        @RequestParam("username") @NotBlank(message = "아이디는 필수 입력입니다.")
-        @Size(min = 5, max = 20, message = "아이디는 5~20자만 가능합니다.")
-        @Pattern(
-            regexp = RegexPatternConstants.LOWERCASE_NUMBER_UNDERSCORE,
-            message = "아이디는 소문자 영어, 숫자, 특수문자(_)의 조합으로 입력해 주세요."
-        )
+        @RequestParam @NotBlank @Size(min = 5, max = 20)
+        @Pattern(regexp = LOWERCASE_NUMBER_UNDERSCORE)
         @Parameter(description = "아이디", example = "photi_123")
         username: String
     ): ResponseEntity<StringResponse> {
@@ -97,26 +75,8 @@ class AuthController(
 
     @PostMapping("/api/users/register")
     @Operation(summary = "회원가입")
-    @ApiResponses(
-        value = [
-            ApiResponse(responseCode = "201", description = "회원가입 성공"),
-            ApiResponse(
-                responseCode = "400",
-                description = """
-                1. 이메일 인증을 먼저 해주세요.
-                2. 비밀번호와 비밀번호 재입력이 동일하지 않습니다.
-                """
-            ),
-            ApiResponse(
-                responseCode = "409",
-                description = """
-                1. 해당 이메일로 이미 가입된 회원이 있습니다.
-                2. 사용 불가능한 아이디입니다.
-                3. 이미 사용중인 아이디입니다.
-                """
-            ),
-        ]
-    )
+    @ApiResponse(responseCode = "201")
+    @ApiErrorResponses([EMAIL_VALIDATION_INVALID, PASSWORD_MATCH_INVALID, EXISTING_USER, UNAVAILABLE_USERNAME, EXISTING_USERNAME])
     fun registerUser(@RequestBody @Valid request: UserRegisterRequest): ResponseEntity<UserRegisterResponse> {
         val response = authService.registerUser(request.toServiceDto())
         val headers = jwtProvider.createToken(response.userId)
@@ -126,12 +86,8 @@ class AuthController(
 
     @PostMapping("/api/users/find-username")
     @Operation(summary = "아이디 찾기")
-    @ApiResponses(
-        value = [
-            ApiResponse(responseCode = "200", description = "아이디 찾기 성공"),
-            ApiResponse(responseCode = "404", description = "존재하지 않는 회원입니다."),
-        ]
-    )
+    @ApiResponse(responseCode = "200")
+    @ApiErrorResponses([USER_NOT_FOUND])
     fun findUsername(@RequestBody @Valid request: UserFindUsernameRequest): ResponseEntity<StringResponse> {
         authService.findUsername(request.toServiceDto())
 
@@ -140,12 +96,8 @@ class AuthController(
 
     @PostMapping("/api/users/find-password")
     @Operation(summary = "비밀번호 찾기")
-    @ApiResponses(
-        value = [
-            ApiResponse(responseCode = "200", description = "비밀번호 찾기 성공"),
-            ApiResponse(responseCode = "404", description = "존재하지 않는 회원입니다."),
-        ]
-    )
+    @ApiResponse(responseCode = "200")
+    @ApiErrorResponses([USER_NOT_FOUND])
     fun findPassword(@RequestBody @Valid request: UserFindPasswordRequest): ResponseEntity<StringResponse> {
         authService.findPassword(request.toServiceDto())
 
@@ -154,12 +106,8 @@ class AuthController(
 
     @PostMapping("/api/users/login")
     @Operation(summary = "로그인")
-    @ApiResponses(
-        value = [
-            ApiResponse(responseCode = "200", description = "로그인 성공"),
-            ApiResponse(responseCode = "401", description = "아이디 또는 비밀번호가 틀렸습니다."),
-        ]
-    )
+    @ApiResponse(responseCode = "200")
+    @ApiErrorResponses([LOGIN_UNAUTHENTICATED])
     fun login(@RequestBody @Valid request: UserLoginRequest): ResponseEntity<UserLoginResponse> {
         val response = authService.login(request.toServiceDto())
         val headers = jwtProvider.createToken(response.userId)
@@ -169,20 +117,8 @@ class AuthController(
 
     @PatchMapping("/api/users/password")
     @Operation(summary = "비밀번호 변경", security = [SecurityRequirement(name = ACCESS_TOKEN_KEY)])
-    @ApiResponses(
-        value = [
-            ApiResponse(responseCode = "200", description = "비밀번호 변경 성공"),
-            ApiResponse(responseCode = "400", description = "비밀번호와 비밀번호 재입력이 동일하지 않습니다."),
-            ApiResponse(
-                responseCode = "401",
-                description = """
-                1. 승인되지 않은 요청입니다. 다시 로그인 해주세요.
-                2. 아이디 또는 비밀번호가 틀렸습니다.
-                """
-            ),
-            ApiResponse(responseCode = "403", description = "권한이 없는 요청입니다. 로그인 후에 다시 시도 해주세요."),
-        ]
-    )
+    @ApiResponse(responseCode = "200")
+    @ApiErrorResponses([PASSWORD_MATCH_INVALID, LOGIN_UNAUTHENTICATED, TOKEN_UNAUTHENTICATED, TOKEN_UNAUTHORIZED])
     fun changePassword(
         principal: Principal,
         @RequestBody @Valid request: UserChangePasswordRequest
@@ -194,13 +130,8 @@ class AuthController(
 
     @PostMapping("/api/users/token")
     @Operation(summary = "토큰 재발급", security = [SecurityRequirement(name = REFRESH_TOKEN_KEY)])
-    @ApiResponses(
-        value = [
-            ApiResponse(responseCode = "200", description = "토큰 재발급 성공"),
-            ApiResponse(responseCode = "401", description = "승인되지 않은 요청입니다. 다시 로그인 해주세요."),
-            ApiResponse(responseCode = "403", description = "권한이 없는 요청입니다. 로그인 후에 다시 시도 해주세요."),
-        ]
-    )
+    @ApiResponse(responseCode = "200")
+    @ApiErrorResponses([TOKEN_UNAUTHENTICATED, TOKEN_UNAUTHORIZED])
     fun refreshToken(principal: Principal): ResponseEntity<StringResponse> {
         val headers = jwtProvider.createToken(UserUtility.getUserId(principal))
 
