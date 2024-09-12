e902f19ef4a0d348694483bdf48b8fc9a185ec99
YuGyeong98 <kuk3097@naver.com>
2024-09-12 15:03:38 +0900

    #103 [test] 모든 챌린지 조회 repository 테스트 코드 구현

--- src/test/kotlin/com/alloon/alloonserver/domain/challenge/ChallengeRepositoryTest.kt
+++ src/test/kotlin/com/alloon/alloonserver/domain/challenge/ChallengeRepositoryTest.kt
@@ -1,14 +1,15 @@
 package com.alloon.alloonserver.domain.challenge
 
 import com.alloon.alloonserver.framework.TestContainerInitializer
-import com.alloon.alloonserver.service.challenge.dto.CreateChallengeDto
 import com.alloon.alloonserver.service.challenge.dto.ChallengeHashtagDto
 import com.alloon.alloonserver.service.challenge.dto.ChallengeRuleDto
+import com.alloon.alloonserver.service.challenge.dto.CreateChallengeDto
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.data.domain.PageRequest
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.transaction.annotation.Transactional
@@ -49,6 +50,33 @@ class ChallengeRepositoryTest(
         assertThat(result).isEqualTo(challenge)
     }
 
+    @DisplayName("챌린지 개최 날짜 기준 최신순으로 정렬된 챌린지 페이징 객체를 반환한다.")
+    @Test
+    fun givenValid_whenFindAllOrderByStartDate_thenReturnSlice() {
+        // given
+        val pageable = PageRequest.of(0, 10)
+        val firstChallenge = getChallenge(2L, LocalDate.of(2025, 9, 1))
+        val secondChallenge = getChallenge(1L, LocalDate.of(2024, 9, 1))
+        val thirdChallenge = getChallenge(5L, LocalDate.of(2024, 7, 30))
+        val challenges = listOf(
+            secondChallenge,
+            firstChallenge,
+            getChallenge(3L, LocalDate.of(2024, 7, 1)),
+            getChallenge(4L, LocalDate.of(2023, 10, 14)),
+            thirdChallenge,
+        )
+
+        challengeRepository.saveAll(challenges)
+
+        // when
+        val result = challengeRepository.findAllOrderByStartDate(pageable)
+
+        // then
+        assertThat(result.content[0].id).isEqualTo(firstChallenge.id)
+        assertThat(result.content[1].id).isEqualTo(secondChallenge.id)
+        assertThat(result.content[2].id).isEqualTo(thirdChallenge.id)
+    }
+
     private fun createAndSaveChallenge(): Challenge {
         val dto = getCreateChallengeDto()
         val challenge = dto.toEntity("https://url.kr/5MhHhD")
@@ -71,6 +99,19 @@ class ChallengeRepositoryTest(
                 ChallengeHashtagDto("해시태그 1"),
                 ChallengeHashtagDto("해시태그 2"),
             )
+        )
+    }
+
+    private fun getChallenge(id: Long, startDate: LocalDate): Challenge {
+        return Challenge(
+            id = id,
+            name = "챌린지 이름",
+            isPublic = true,
+            goal = "챌린지 목표입니다.",
+            proveTime = LocalTime.of(13, 0),
+            endDate = LocalDate.of(2024, 12, 1),
+            imageUrl = "https://url.kr/5MhHhD",
+            startDate = startDate,
         )
     }
 }
