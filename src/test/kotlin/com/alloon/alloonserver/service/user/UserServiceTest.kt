94a18d5a876ab322d14b50514cc06111a9678428
YuGyeong98 <kuk3097@naver.com>
2024-09-18 13:28:01 +0900

    #106 [test] 사용자 프로필 이미지 업로드 service 테스트 코드 구현

--- src/test/kotlin/com/alloon/alloonserver/service/user/UserServiceTest.kt
+++ src/test/kotlin/com/alloon/alloonserver/service/user/UserServiceTest.kt
@@ -2,19 +2,25 @@ package com.alloon.alloonserver.service.user
 
 import com.alloon.alloonserver.common.constant.ExceptionCode.USER_NOT_FOUND
 import com.alloon.alloonserver.common.response.CustomException
+import com.alloon.alloonserver.domain.user.Contact
+import com.alloon.alloonserver.domain.user.User
 import com.alloon.alloonserver.domain.user.UserRepository
 import com.alloon.alloonserver.framework.TestContainerInitializer
 import com.alloon.alloonserver.service.s3.S3Service
 import com.alloon.alloonserver.service.user.dto.FindUserInfoDto
+import io.mockk.Runs
 import io.mockk.every
+import io.mockk.just
 import io.mockk.mockk
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import org.springframework.mock.web.MockMultipartFile
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.transaction.annotation.Transactional
+import java.util.*
 
 @ActiveProfiles("test")
 @Transactional
@@ -31,7 +37,7 @@ class UserServiceTest {
     fun givenValid_whenFindUserInfo_thenReturn() {
         // given
         val userId = 1L
-        val dto = FindUserInfoDto("https://url.kr/5MhHhD", "tester", "tester@photi.com")
+        val dto = getUserInfoDto()
 
         every { userRepository.findInfoById(any()) } returns dto
 
@@ -57,33 +63,54 @@ class UserServiceTest {
             .isEqualTo(USER_NOT_FOUND)
     }
 
-//    @DisplayName("회원 이미지 업로드가 정상 작동한다")
-//    @Test
-//    fun givenValid_whenUploadImage_thenReturn() {
-//        // given
-//        val contact = createAndSaveContact()
-//        val user = createAndSaveUser(contact)
-//        val file = MockMultipartFile("file", "file.png", "image/png", ByteArray(1))
-//
-//        // when
-//        val response = userService.uploadImage(user.id!!, file)
-//
-//        // then
-//        assertThat(response)
-//            .extracting("userId", "username", "imageUrl", "email")
-//            .containsExactly(user.id, user.username, user.imageUrl, contact.email)
-//    }
-//
-//    @DisplayName("존재하지 않은 회원 식별자로 회원 이미지 업로드를 하면 예외가 발생한다")
-//    @Test
-//    fun givenNonExistingUserId_whenUploadImage_thenThrow() {
-//        // given
-//        val file = MockMultipartFile("file", "file.png", "image/png", ByteArray(1))
-//
-//        // when & then
-//        assertThatThrownBy { userService.uploadImage(1L, file) }
-//            .isInstanceOf(CustomException::class.java)
-//            .extracting("exceptionCode")
-//            .isEqualTo(USER_NOT_FOUND)
-//    }
+    @DisplayName("사용자 프로필 이미지 업로드를 하면 일치하는 사용자 정보를 반환한다.")
+    @Test
+    fun givenValid_whenUpdateUserImage_thenReturn() {
+        // given
+        val userId = 1L
+        val multipartFile = getMultipartFile()
+        val user = getUser()
+        val imageUrl = "https://url.kr/5MhHhD"
+        val dto = getUserInfoDto()
+
+        every { userRepository.findById(any()) } returns Optional.of(user)
+        every { s3Service.deleteImage(any(), any()) } just Runs
+        every { s3Service.uploadImage(any(), any()) } returns ""
+        every { s3Service.getImageUrl(any()) } returns imageUrl
+
+        // when
+        val result = userService.updateUserImage(userId, multipartFile)
+
+        // then
+        assertThat(result).isEqualTo(dto)
+    }
+
+    @DisplayName("존재하지 않은 사용자로 이미지 업로드를 하면 예외가 발생한다.")
+    @Test
+    fun givenNotFoundUser_whenUpdateUserImage_thenThrow() {
+        // given
+        val userId = 1L
+        val multipartFile = getMultipartFile()
+
+        every { userRepository.findById(any()) } returns Optional.empty()
+
+        // when & then
+        assertThatThrownBy { userService.updateUserImage(userId, multipartFile) }
+            .isInstanceOf(CustomException::class.java)
+            .extracting("exceptionCode")
+            .isEqualTo(USER_NOT_FOUND)
+    }
+
+    private fun getUser(): User {
+        val contact = Contact(1L, "tester@photi.com", "000000", true)
+        return User(1L, contact, "tester", "password1!", "")
+    }
+
+    private fun getUserInfoDto(): FindUserInfoDto {
+        return FindUserInfoDto("https://url.kr/5MhHhD", "tester", "tester@photi.com")
+    }
+
+    private fun getMultipartFile(): MockMultipartFile {
+        return MockMultipartFile("imageFile", "file.png", "image/png", ByteArray(1))
+    }
 }
