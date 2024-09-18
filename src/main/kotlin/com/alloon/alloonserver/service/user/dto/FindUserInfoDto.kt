c7fe69e9492ebfcdc71717b353c7fc8c764e067b
YuGyeong98 <kuk3097@naver.com>
2024-09-18 13:01:31 +0900

    #106 [feat] 사용자 프로필 이미지 업로드 service dto 구현

--- src/main/kotlin/com/alloon/alloonserver/service/user/dto/FindUserInfoDto.kt
+++ src/main/kotlin/com/alloon/alloonserver/service/user/dto/FindUserInfoDto.kt
@@ -1,9 +1,18 @@
 package com.alloon.alloonserver.service.user.dto
 
+import com.alloon.alloonserver.domain.user.User
 import com.querydsl.core.annotations.QueryProjection
 
 data class FindUserInfoDto @QueryProjection constructor(
     val imageUrl: String,
     val username: String,
     val email: String,
-)
+) {
+
+    companion object {
+
+        fun of(user: User): FindUserInfoDto {
+            return FindUserInfoDto(user.imageUrl, user.username, user.contact.email)
+        }
+    }
+}
