9e53a511bbda7e614a5b2b18370c9315741f4837
YuGyeong98 <kuk3097@naver.com>
2024-09-30 17:52:34 +0900

    #117 [feat] 스케줄링 활성화 어노테이션 추가

--- src/main/kotlin/com/alloon/alloonserver/AlloonServerApplication.kt
+++ src/main/kotlin/com/alloon/alloonserver/AlloonServerApplication.kt
@@ -2,10 +2,12 @@ package com.alloon.alloonserver
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
+@EnableScheduling
 class AlloonServerApplication
 
 fun main(args: Array<String>) {
-	runApplication<AlloonServerApplication>(*args)
+    runApplication<AlloonServerApplication>(*args)
 }
