2589405478fcdc4026608dc0030fe63a1aa4c019
YuGyeong98 <kuk3097@naver.com>
2024-10-13 00:36:51 +0900

    #132 [refactor] 작성자 피드 댓글 조회 메소드 매개변수 이름 변경

--- src/main/kotlin/com/alloon/alloonserver/domain/feed/FeedCommentRepository.kt
+++ src/main/kotlin/com/alloon/alloonserver/domain/feed/FeedCommentRepository.kt
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository
 
 interface FeedCommentRepository : JpaRepository<FeedComment, Long> {
 
-    fun findByChallengeMemberIdAndFeedId(challengeId: Long?, feedId: Long): FeedComment?
+    fun findByChallengeMemberIdAndFeedId(challengeMemberId: Long?, feedId: Long): FeedComment?
 }
