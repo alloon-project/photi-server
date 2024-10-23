8a16c6b4de09fa5e93f9ad2daf9336d28943a106
YuGyeong98 <kuk3097@naver.com>
2024-10-23 17:44:11 +0900

    #150 [feat] 올바르지 않은 날짜 형식 예외 처리

--- src/main/kotlin/com/alloon/alloonserver/common/exception/CustomExceptionHandler.kt
+++ src/main/kotlin/com/alloon/alloonserver/common/exception/CustomExceptionHandler.kt
@@ -1,12 +1,13 @@
 package com.alloon.alloonserver.common.exception
 
-import com.alloon.alloonserver.common.constant.ExceptionCode.FILE_SIZE_EXCEED
-import com.alloon.alloonserver.common.constant.ExceptionCode.SERVER_ERROR
+import com.alloon.alloonserver.common.constant.ExceptionCode
+import com.alloon.alloonserver.common.constant.ExceptionCode.*
 import com.alloon.alloonserver.common.response.CustomException
 import com.alloon.alloonserver.common.response.ErrorResponse
 import io.sentry.Sentry
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.validation.ConstraintViolationException
+import org.springframework.beans.TypeMismatchException
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus.*
 import org.springframework.http.HttpStatusCode
@@ -16,8 +17,11 @@ import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 import org.springframework.web.context.request.WebRequest
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException
 import org.springframework.web.multipart.MaxUploadSizeExceededException
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
+import java.time.format.DateTimeParseException
+
 
 @RestControllerAdvice
 class CustomExceptionHandler : ResponseEntityExceptionHandler() {
@@ -83,6 +87,16 @@ class CustomExceptionHandler : ResponseEntityExceptionHandler() {
         val message = ex.mostSpecificCause.message.toString()
         val response = ErrorResponse(status.toString().split(" ")[1], message)
 
+        return ResponseEntity.status(BAD_REQUEST).body(response)
+    }
+
+    override fun handleTypeMismatch(
+        ex: TypeMismatchException,
+        headers: HttpHeaders,
+        status: HttpStatusCode,
+        request: WebRequest
+    ): ResponseEntity<Any>? {
+        val response = ErrorResponse(DATE_FORMAT_INVALID.name, DATE_FORMAT_INVALID.message)
         return ResponseEntity.status(BAD_REQUEST).body(response)
     }
 
