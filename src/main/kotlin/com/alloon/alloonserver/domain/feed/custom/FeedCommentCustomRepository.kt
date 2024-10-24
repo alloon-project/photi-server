fe92861cd79ccba36ad5782c2e35775623ab6d27
YuGyeong98 <kuk3097@naver.com>
2024-10-24 10:03:46 +0900

    #142 [feat] 피드 댓글 리스트 조회 repository 메소드 구현

--- src/main/kotlin/com/alloon/alloonserver/domain/feed/custom/FeedCommentCustomRepository.kt
+++ src/main/kotlin/com/alloon/alloonserver/domain/feed/custom/FeedCommentCustomRepository.kt
@@ -1,8 +1,13 @@
 package com.alloon.alloonserver.domain.feed.custom
 
 import com.alloon.alloonserver.domain.feed.FeedComment
+import com.alloon.alloonserver.service.challenge.dto.FindChallengeFeedCommentsDto
+import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Slice
 
 interface FeedCommentCustomRepository {
 
     fun findByCommentId(commentId: Long): FeedComment?
+
+    fun findAllByFeedId(feedId: Long, pageable: Pageable): Slice<FindChallengeFeedCommentsDto>
 }
