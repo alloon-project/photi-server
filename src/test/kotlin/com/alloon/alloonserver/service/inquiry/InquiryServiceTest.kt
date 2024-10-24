8b27cc3ab8d5900d15930844f271f70a639f81ce
YuGyeong98 <kuk3097@naver.com>
2024-10-24 13:24:12 +0900

    #149 [test] 문의하기 service 예외 테스트 코드 추가

--- src/test/kotlin/com/alloon/alloonserver/service/inquiry/InquiryServiceTest.kt
+++ src/test/kotlin/com/alloon/alloonserver/service/inquiry/InquiryServiceTest.kt
@@ -1,5 +1,7 @@
 package com.alloon.alloonserver.service.inquiry
 
+import com.alloon.alloonserver.common.constant.ExceptionCode.USER_NOT_FOUND
+import com.alloon.alloonserver.common.response.CustomException
 import com.alloon.alloonserver.domain.inquiry.InquiryCategoryType
 import com.alloon.alloonserver.domain.inquiry.InquiryRepository
 import com.alloon.alloonserver.domain.user.Contact
@@ -9,6 +11,7 @@ import com.alloon.alloonserver.service.inquiry.dto.CreateInquiryDto
 import io.mockk.every
 import io.mockk.mockk
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.springframework.transaction.annotation.Transactional
@@ -39,6 +42,22 @@ class InquiryServiceTest {
         // then
         assertThat(inquiry.type).isEqualTo(InquiryCategoryType.valueOf(dto.type))
         assertThat(inquiry.content).isEqualTo(dto.content)
+    }
+
+    @DisplayName("등록되지 않은 사용자가 문의하기를 하면 예외가 발생한다.")
+    @Test
+    fun givenNotFoundUser_whenCreateInquiry_thenThrow() {
+        // given
+        val userId = 1L
+        val dto = CreateInquiryDto("SERVICE_USE", "서비스 이용 관련 문의입니다.")
+
+        every { userRepository.find(any()) } returns null
+
+        // when & then
+        assertThatThrownBy { inquiryService.createInquiry(userId, dto) }
+            .isInstanceOf(CustomException::class.java)
+            .extracting("exceptionCode")
+            .isEqualTo(USER_NOT_FOUND)
     }
 
     private fun getUser(): User {
