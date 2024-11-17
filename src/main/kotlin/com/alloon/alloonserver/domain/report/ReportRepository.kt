e07069abde96fe4a609bafe0b3af227c0d5794f5
YuGyeong98 <kuk3097@naver.com>
2024-11-17 18:13:31 +0900

    #168 [feat] 신고 등록 service 구현

--- src/main/kotlin/com/alloon/alloonserver/domain/report/ReportRepository.kt
+++ src/main/kotlin/com/alloon/alloonserver/domain/report/ReportRepository.kt
@@ -1,7 +1,6 @@
 package com.alloon.alloonserver.domain.report
 
-import com.alloon.alloonserver.domain.report.custom.ReportRepositoryCustom
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface ReportRepository : JpaRepository<Report, Long>, ReportRepositoryCustom {
+interface ReportRepository : JpaRepository<Report, Long> {
 }
--- src/main/kotlin/com/alloon/alloonserver/service/report/ReportService.kt
+++ src/main/kotlin/com/alloon/alloonserver/service/report/ReportService.kt
@@ -2,25 +2,20 @@ package com.alloon.alloonserver.service.report
 
 import com.alloon.alloonserver.common.constant.ExceptionCode.*
 import com.alloon.alloonserver.common.response.CustomException
-import com.alloon.alloonserver.domain.feed.FeedRepository
 import com.alloon.alloonserver.domain.challenge.ChallengeMemberRepository
 import com.alloon.alloonserver.domain.challenge.ChallengeRepository
-import com.alloon.alloonserver.domain.report.ReportCategoryRepository
+import com.alloon.alloonserver.domain.feed.FeedRepository
 import com.alloon.alloonserver.domain.report.ReportCategoryType
 import com.alloon.alloonserver.domain.report.ReportCategoryType.*
 import com.alloon.alloonserver.domain.report.ReportRepository
 import com.alloon.alloonserver.domain.user.UserRepository
-import com.alloon.alloonserver.service.report.dto.ReportCreateServiceDto
-import jakarta.validation.Valid
+import com.alloon.alloonserver.service.report.dto.CreateReportDto
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
-import org.springframework.validation.annotation.Validated
 
-@Validated
 @Service
 @Transactional(readOnly = true)
 class ReportService(
-    private val reportCategoryRepository: ReportCategoryRepository,
     private val reportRepository: ReportRepository,
     private val challengeRepository: ChallengeRepository,
     private val challengeMemberRepository: ChallengeMemberRepository,
@@ -28,34 +23,19 @@ class ReportService(
     private val userRepository: UserRepository,
 ) {
 
-    fun getAllReportCategoryDescription(reportType: String): List<String> {
-        return reportCategoryRepository.findAllDescription(ReportCategoryType.valueOf(reportType))
-    }
+    @Transactional
+    fun createReport(userId: Long, targetId: Long, dto: CreateReportDto) {
+        userRepository.find(userId) ?: throw CustomException(USER_NOT_FOUND)
 
-    fun createReport(userId: Long, @Valid request: ReportCreateServiceDto) {
-        val reporter = userRepository.find(userId) ?: throw CustomException(USER_NOT_FOUND)
+        when (ReportCategoryType.valueOf(dto.category)) {
+            CHALLENGE -> challengeRepository.find(targetId)
+                ?: throw CustomException(CHALLENGE_NOT_FOUND)
 
-        val reportCategory = reportCategoryRepository.find(
-            request.reportCategoryId, ReportCategoryType.valueOf(request.reportType)
-        ) ?: throw CustomException(REPORT_CATEGORY_NOT_FOUND)
+            CHALLENGE_MEMBER -> challengeMemberRepository.find(targetId)
+                ?: throw CustomException(CHALLENGE_MEMBER_NOT_FOUND)
 
-        when (ReportCategoryType.valueOf(request.reportType)) {
-            CHALLENGE -> {
-                val challenge =
-                    challengeRepository.find(request.reportTargetId) ?: throw CustomException(CHALLENGE_NOT_FOUND)
-                reportRepository.save(request.toEntity(reportCategory, reporter, challenge = challenge))
-            }
-
-            CHALLENGE_MEMBER -> {
-                val challengeMember = challengeMemberRepository.find(request.reportTargetId)
-                    ?: throw CustomException(CHALLENGE_MEMBER_NOT_FOUND)
-                reportRepository.save(request.toEntity(reportCategory, reporter, challengeMember = challengeMember))
-            }
-
-            FEED -> {
-                val feed = feedRepository.find(request.reportTargetId) ?: throw CustomException(FEED_NOT_FOUND)
-                reportRepository.save(request.toEntity(reportCategory, reporter, feed = feed))
-            }
+            FEED -> feedRepository.find(targetId) ?: throw CustomException(FEED_NOT_FOUND)
         }
+        reportRepository.save(dto.toReportEntity(userId, targetId))
     }
 }
