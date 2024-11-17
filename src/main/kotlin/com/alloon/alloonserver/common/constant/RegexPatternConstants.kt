233df3b229d435f087d9e48079d649a2ae3f2069
YuGyeong98 <kuk3097@naver.com>
2024-11-17 18:14:06 +0900

    #168 [feat] 신고 등록 request 구현

--- src/main/kotlin/com/alloon/alloonserver/common/constant/RegexPatternConstants.kt
+++ src/main/kotlin/com/alloon/alloonserver/common/constant/RegexPatternConstants.kt
@@ -5,6 +5,5 @@ class RegexPatternConstants {
     companion object {
         const val LOWERCASE_NUMBER_UNDERSCORE = "^[a-z0-9_]+$"
         const val LETTER_NUMBER_SPECIAL_CHARACTER = "^(?=.*[A-z])(?=.*\\d)(?=.*[#$@!%&*])[A-z\\d#$@!%&*]+$"
-        const val REPORT_CATEGORY_TYPE_CHARACTER = "^(CHALLENGE|CHALLENGE_MEMBER|FEED)"
     }
 }
