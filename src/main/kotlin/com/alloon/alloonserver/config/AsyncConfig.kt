bfb7447424bba36a52c4565825ea3d5a23de7439
YuGyeong98 <kuk3097@naver.com>
2024-12-07 00:38:18 +0900

    #158 [refactor] async config 수정

--- src/main/kotlin/com/alloon/alloonserver/config/AsyncConfig.kt
+++ src/main/kotlin/com/alloon/alloonserver/config/AsyncConfig.kt
@@ -1,17 +1,28 @@
 package com.alloon.alloonserver.config
 
+import com.alloon.alloonserver.common.exception.CustomAsyncUncaughtExceptionHandler
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler
 import org.springframework.context.annotation.Configuration
 import org.springframework.scheduling.annotation.AsyncConfigurer
 import org.springframework.scheduling.annotation.EnableAsync
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor
 import java.util.concurrent.Executor
-import java.util.concurrent.LinkedBlockingQueue
-import java.util.concurrent.ThreadPoolExecutor
-import java.util.concurrent.TimeUnit
 
 @EnableAsync
 @Configuration
 class AsyncConfig : AsyncConfigurer {
 
-    override fun getAsyncExecutor(): Executor =
-        ThreadPoolExecutor(10, 30, 30, TimeUnit.SECONDS, LinkedBlockingQueue())
+    override fun getAsyncExecutor(): Executor? {
+        return ThreadPoolTaskExecutor().apply {
+            corePoolSize = 3
+            maxPoolSize = 10
+            queueCapacity = 500
+            setThreadNamePrefix("Executor-")
+            initialize()
+        }
+    }
+
+    override fun getAsyncUncaughtExceptionHandler(): AsyncUncaughtExceptionHandler {
+        return CustomAsyncUncaughtExceptionHandler()
+    }
 }
