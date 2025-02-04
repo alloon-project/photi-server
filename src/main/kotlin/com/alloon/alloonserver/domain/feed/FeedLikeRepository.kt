cffad71435f6a6354aae1e3f3f7f92b0b5c09368
YuGyeong98 <kuk3097@naver.com>
2025-02-04 23:11:19 +0900

    #180 [feat] 챌린지 피드 좋아요 취소 repository 구현

--- src/main/kotlin/com/alloon/alloonserver/domain/feed/FeedLikeRepository.kt
+++ src/main/kotlin/com/alloon/alloonserver/domain/feed/FeedLikeRepository.kt
@@ -1,6 +1,9 @@
 package com.alloon.alloonserver.domain.feed
 
+import com.alloon.alloonserver.domain.challenge.ChallengeMember
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface FeedLikeRepository : JpaRepository<FeedLike, Long> {
+
+    fun findByFeedIdAndChallengeMember(feedId: Long, challengeMember: ChallengeMember): FeedLike?
 }
