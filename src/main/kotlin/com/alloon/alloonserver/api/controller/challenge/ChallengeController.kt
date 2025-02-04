000634802adba86f6967395b7ab390717ff339b8
YuGyeong98 <kuk3097@naver.com>
2025-02-04 22:45:42 +0900

    #180 [feat] 챌린지 피드 좋아요 controller 구현

--- src/main/kotlin/com/alloon/alloonserver/api/controller/challenge/ChallengeController.kt
+++ src/main/kotlin/com/alloon/alloonserver/api/controller/challenge/ChallengeController.kt
@@ -429,4 +429,18 @@ class ChallengeController(
 
         return ResponseEntity.ok(StringSuccessResponse("비공개 챌린지 참여하기가 완료되었습니다."))
     }
+
+    @PostMapping("/{challengeId}/feeds/{feedId}/like")
+    @Operation(summary = "챌린지 피드 좋아요", security = [SecurityRequirement(name = ACCESS_TOKEN_KEY)])
+    @ApiResponse(responseCode = "201")
+    @ApiErrorResponses([TOKEN_UNAUTHENTICATED, TOKEN_UNAUTHORIZED, CHALLENGE_MEMBER_NOT_FOUND, CHALLENGE_NOT_FOUND, FEED_NOT_FOUND])
+    fun likeChallengeFeed(
+        principal: Principal,
+        @PathVariable @Parameter(description = "챌린지 id", example = "1") challengeId: Long,
+        @PathVariable @Parameter(description = "피드 id", example = "1") feedId: Long,
+    ): ResponseEntity<StringSuccessResponse> {
+        challengeService.likeChallengeFeed(UserUtility.getUserId(principal), challengeId, feedId)
+
+        return ResponseEntity.ok(StringSuccessResponse("챌린지 피드 좋아요가 완료되었습니다."))
+    }
 }
