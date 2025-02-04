cc439434f5e2c93f9697671a31a4bbcc742cbefc
YuGyeong98 <kuk3097@naver.com>
2025-02-04 23:12:11 +0900

    #180 [feat] 챌린지 피드 좋아요 취소 controller 구현

--- src/main/kotlin/com/alloon/alloonserver/api/controller/challenge/ChallengeController.kt
+++ src/main/kotlin/com/alloon/alloonserver/api/controller/challenge/ChallengeController.kt
@@ -434,13 +434,35 @@ class ChallengeController(
     @Operation(summary = "챌린지 피드 좋아요", security = [SecurityRequirement(name = ACCESS_TOKEN_KEY)])
     @ApiResponse(responseCode = "201")
     @ApiErrorResponses([TOKEN_UNAUTHENTICATED, TOKEN_UNAUTHORIZED, CHALLENGE_MEMBER_NOT_FOUND, CHALLENGE_NOT_FOUND, FEED_NOT_FOUND])
-    fun likeChallengeFeed(
+    fun createChallengeFeedLike(
         principal: Principal,
         @PathVariable @Parameter(description = "챌린지 id", example = "1") challengeId: Long,
         @PathVariable @Parameter(description = "피드 id", example = "1") feedId: Long,
     ): ResponseEntity<StringSuccessResponse> {
-        challengeService.likeChallengeFeed(UserUtility.getUserId(principal), challengeId, feedId)
+        challengeService.createChallengeFeedLike(
+            UserUtility.getUserId(principal),
+            challengeId,
+            feedId
+        )
 
         return ResponseEntity.ok(StringSuccessResponse("챌린지 피드 좋아요가 완료되었습니다."))
+    }
+
+    @DeleteMapping("/{challengeId}/feeds/{feedId}/like")
+    @Operation(summary = "챌린지 피드 좋아요 취소", security = [SecurityRequirement(name = ACCESS_TOKEN_KEY)])
+    @ApiResponse(responseCode = "200")
+    @ApiErrorResponses([TOKEN_UNAUTHENTICATED, TOKEN_UNAUTHORIZED, CHALLENGE_MEMBER_NOT_FOUND, CHALLENGE_NOT_FOUND, FEED_NOT_FOUND, FEED_LIKE_NOT_FOUND])
+    fun deleteChallengeFeedLike(
+        principal: Principal,
+        @PathVariable @Parameter(description = "챌린지 id", example = "1") challengeId: Long,
+        @PathVariable @Parameter(description = "피드 id", example = "1") feedId: Long,
+    ): ResponseEntity<StringSuccessResponse> {
+        challengeService.deleteChallengeFeedLike(
+            UserUtility.getUserId(principal),
+            challengeId,
+            feedId
+        )
+
+        return ResponseEntity.ok(StringSuccessResponse("챌린지 피드 좋아요가 취소되었습니다."))
     }
 }
