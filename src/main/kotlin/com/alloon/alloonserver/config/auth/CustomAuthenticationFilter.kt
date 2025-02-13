0ba16789b387e7048f36d06a19f2595943b97070
YuGyeong98 <kuk3097@naver.com>
2025-02-13 15:53:59 +0900

    [refactor] AuthenticationFilter 수정

--- src/main/kotlin/com/alloon/alloonserver/config/auth/CustomAuthenticationFilter.kt
+++ src/main/kotlin/com/alloon/alloonserver/config/auth/CustomAuthenticationFilter.kt
@@ -1,15 +1,18 @@
 package com.alloon.alloonserver.config.auth
 
+import com.alloon.alloonserver.common.response.CustomException
 import jakarta.servlet.FilterChain
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import org.springframework.http.HttpHeaders.AUTHORIZATION
+import org.springframework.security.authentication.BadCredentialsException
 import org.springframework.stereotype.Component
 import org.springframework.web.filter.OncePerRequestFilter
 
 @Component
 class CustomAuthenticationFilter(
     private val jwtProvider: JwtProvider,
+    private val customAuthenticationEntryPoint: CustomAuthenticationEntryPoint,
 ) : OncePerRequestFilter() {
 
     override fun doFilterInternal(
@@ -17,15 +20,24 @@ class CustomAuthenticationFilter(
         response: HttpServletResponse,
         filterChain: FilterChain
     ) {
-        val accessToken = request.getHeader(AUTHORIZATION)
-        val refreshToken = request.getHeader("Refresh-Token")
+        try {
+            val accessToken = request.getHeader(AUTHORIZATION)
+            val refreshToken = request.getHeader("Refresh-Token")
 
-        if (accessToken != null && !request.requestURI.contains("token")) {
-            jwtProvider.verifyToken(accessToken, JwtType.ACCESS)
-        } else if (refreshToken != null) {
-            jwtProvider.verifyToken(refreshToken, JwtType.REFRESH)
+            if (accessToken != null && !request.requestURI.contains("token")) {
+                jwtProvider.verifyToken(accessToken, JwtType.ACCESS)
+            } else if (refreshToken != null) {
+                jwtProvider.verifyToken(refreshToken, JwtType.REFRESH)
+            }
+
+            filterChain.doFilter(request, response)
+
+        } catch (ex: CustomException) {
+            customAuthenticationEntryPoint.commence(
+                request,
+                response,
+                BadCredentialsException(ex.message)
+            )
         }
-
-        filterChain.doFilter(request, response)
     }
 }
