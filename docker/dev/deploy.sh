160176f2b96f494ef30878d644e2d14bc995706b
YuGyeong98 <kuk3097@naver.com>
2025-04-29 22:45:55 +0900

    #188 [chore] 스크립트 docker-compose 파일 이름 수정

--- docker/dev/deploy.sh
+++ docker/dev/deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 DOCKER_APP_NAME=spring-photi
 
-EXIST_RUNNING=$(docker-compose -p "${DOCKER_APP_NAME}" -f docker-compose.dev.yml ps | grep -E "Up|running")
+EXIST_RUNNING=$(docker-compose -p "${DOCKER_APP_NAME}" -f docker-compose.yml ps | grep -E "Up|running")
 
 echo "deploy start - $(date "+%Y-%m-%d %H:%M:%S")"
 
 if [ -z "$EXIST_RUNNING" ]; then
-  docker-compose -p "${DOCKER_APP_NAME}" -f docker-compose.dev.yml up -d --build
+  docker-compose -p "${DOCKER_APP_NAME}" -f docker-compose.yml up -d --build
   docker image prune -af
 fi
   echo "deploy end - $(date '+%Y-%m-%d %H:%M:%S')"
