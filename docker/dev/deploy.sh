33960cb372124d40268a6d5bb0b31e960473a103
YuGyeong98 <kuk3097@naver.com>
2025-04-30 02:00:04 +0900

    #188 [chore] 매번 재시작 옵션 제거

--- docker/dev/deploy.sh
+++ docker/dev/deploy.sh
@@ -4,7 +4,7 @@ PROJECT_PATH=/home/ubuntu/photi-server
 
 echo "deploy start - $(date "+%Y-%m-%d %H:%M:%S")"
 
-docker compose -p "${DOCKER_APP_NAME}" -f $PROJECT_PATH/docker-compose.yml up -d --build
+docker compose -p "${DOCKER_APP_NAME}" -f $PROJECT_PATH/docker-compose.yml up -d
 docker image prune -af
 
 echo "deploy end - $(date '+%Y-%m-%d %H:%M:%S')"
