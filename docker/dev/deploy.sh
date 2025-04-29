c4abe5faf5276cdc3341bb0c8f977880635dacf0
YuGyeong98 <kuk3097@naver.com>
2025-04-29 22:57:48 +0900

    #188 [chore] 스크립트 docker-compose 파일 경로 수정

--- docker/dev/deploy.sh
+++ docker/dev/deploy.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 DOCKER_APP_NAME=spring-photi
+PROJECT_PATH=/home/ubuntu/photi-server
 
-EXIST_RUNNING=$(docker-compose -p "${DOCKER_APP_NAME}" -f docker-compose.yml ps | grep -E "Up|running")
+EXIST_RUNNING=$(docker-compose -p "${DOCKER_APP_NAME}" -f $PROJECT_PATH/docker-compose.yml ps | grep -E "Up|running")
 
 echo "deploy start - $(date "+%Y-%m-%d %H:%M:%S")"
 
 if [ -z "$EXIST_RUNNING" ]; then
-  docker-compose -p "${DOCKER_APP_NAME}" -f docker-compose.yml up -d --build
+  docker-compose -p "${DOCKER_APP_NAME}" -f $PROJECT_PATH/docker-compose.yml up -d --build
   docker image prune -af
 fi
   echo "deploy end - $(date '+%Y-%m-%d %H:%M:%S')"
