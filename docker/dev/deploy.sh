72b1bd94bfbec7824502abafe2bac83ce64eae10
YuGyeong98 <kuk3097@naver.com>
2025-05-02 17:50:56 +0900

    [chore] 매번 재시작 옵션 추가

--- docker/dev/deploy.sh
+++ docker/dev/deploy.sh
@@ -4,7 +4,7 @@ PROJECT_PATH=/home/ubuntu/photi-server
 
 echo "deploy start - $(date "+%Y-%m-%d %H:%M:%S")"
 
-docker compose -p "${DOCKER_APP_NAME}" -f $PROJECT_PATH/docker-compose.yml up -d
+docker compose -p "${DOCKER_APP_NAME}" -f $PROJECT_PATH/docker-compose.yml up -d --build
 docker image prune -af
 
 echo "deploy end - $(date '+%Y-%m-%d %H:%M:%S')"
