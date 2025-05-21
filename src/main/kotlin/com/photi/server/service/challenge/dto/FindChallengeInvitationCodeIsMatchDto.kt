b80cc23b1f3b817e8ce7b92e9cfc9ab1e7d07144
YuGyeong98 <kuk3097@naver.com>
2025-05-21 19:29:21 +0900

    #193 [feat] 챌린지 초대코드 일치 여부 조회 request 구현

--- src/main/kotlin/com/photi/server/service/challenge/dto/FindChallengeInvitationCodeIsMatchDto.kt
+++ src/main/kotlin/com/photi/server/service/challenge/dto/FindChallengeInvitationCodeIsMatchDto.kt
@@ -1,14 +1,5 @@
 package com.photi.server.service.challenge.dto
 
-import com.photi.server.domain.challenge.Challenge
-import com.photi.server.domain.challenge.ChallengeMember
-import com.photi.server.domain.user.User
-
-data class FindChallengeInvitationCodeIsValidDto(
+data class FindChallengeInvitationCodeIsMatchDto(
     val invitationCode: String,
-) {
-
-    fun toEntity(user: User, challenge: Challenge): ChallengeMember {
-        return ChallengeMember(user = user, challenge = challenge, isCreator = false)
-    }
-}
+)
