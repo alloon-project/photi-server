ce3b7b8add95162ce8ed412b594d16078fac5f5d
YuGyeong98 <kuk3097@naver.com>
2025-07-17 16:19:39 +0900

    #247 [refactor] 메일 from 주소 이름 추가

--- src/main/kotlin/com/photi/server/service/email/EmailService.kt
+++ src/main/kotlin/com/photi/server/service/email/EmailService.kt
@@ -3,6 +3,7 @@ package com.photi.server.service.email
 import com.photi.server.common.constant.EmailConstants
 import com.photi.server.common.constant.ExceptionCode.EMAIL_SEND_ERROR
 import com.photi.server.common.response.CustomException
+import jakarta.mail.internet.InternetAddress
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.mail.MailSendException
 import org.springframework.mail.javamail.JavaMailSender
@@ -13,34 +14,28 @@ import org.thymeleaf.context.Context
 import org.thymeleaf.spring6.SpringTemplateEngine
 
 @Service
-class EmailService (
-    @Value("\${api.mail.address}")
+class EmailService(
+    @Value("\${spring.mail.username}")
     private val fromAddress: String,
-
     private val mailSender: JavaMailSender,
-    private val templateEngine: SpringTemplateEngine
+    private val templateEngine: SpringTemplateEngine,
 ) {
 
-    /**
-     * 이메일 전송 |
-     * @param toAddress 받는 주소
-     * @param key 인증 코드
-     * @param constants 이메일 종류
-     * @throws EMAIL_SEND_ERROR 500
-     */
     @Async
     fun sendEmail(toAddress: String, key: String, constants: EmailConstants) {
         val context = Context().apply {
-            setVariables(mapOf(
-                "intro" to constants.content,
-                "key" to key,
-            ))
+            setVariables(
+                mapOf(
+                    "intro" to constants.content,
+                    "key" to key,
+                )
+            )
         }
         val mimeMessage = mailSender.createMimeMessage()
         val mimeMessageHelper = MimeMessageHelper(mimeMessage, "utf-8")
 
         with(mimeMessageHelper) {
-            setFrom(fromAddress)
+            setFrom(InternetAddress(fromAddress, "포티"))
             setTo(toAddress)
             setSubject(constants.subject)
             setText(templateEngine.process("mail", context), true)
