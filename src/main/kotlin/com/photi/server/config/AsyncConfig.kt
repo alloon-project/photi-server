1540838407f65e55e8808bfc2987d59376cdc34b
YuGyeong98 <kuk3097@naver.com>
2025-09-13 15:32:50 +0900

    #263 [refactor] graceful shutdown 설정 및 비동기 설정 수정

--- src/main/kotlin/com/photi/server/config/AsyncConfig.kt
+++ src/main/kotlin/com/photi/server/config/AsyncConfig.kt
@@ -1,24 +1,31 @@
 package com.photi.server.config
 
 import com.photi.server.common.exception.CustomAsyncUncaughtExceptionHandler
+import com.photi.server.common.log.LoggingTaskDecorator
 import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler
 import org.springframework.context.annotation.Configuration
 import org.springframework.scheduling.annotation.AsyncConfigurer
 import org.springframework.scheduling.annotation.EnableAsync
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor
-import java.util.concurrent.Executor
+import java.util.concurrent.ThreadPoolExecutor
 
 @EnableAsync
 @Configuration
 class AsyncConfig : AsyncConfigurer {
 
-    override fun getAsyncExecutor(): Executor? {
+    override fun getAsyncExecutor(): ThreadPoolTaskExecutor {
         return ThreadPoolTaskExecutor().apply {
-            corePoolSize = 3
-            maxPoolSize = 10
-            queueCapacity = 500
+            corePoolSize = 10
+            queueCapacity = 50
+            maxPoolSize = 30
+            keepAliveSeconds = 60
             setThreadNamePrefix("Executor-")
-            initialize()
+            setTaskDecorator(LoggingTaskDecorator())
+            setRejectedExecutionHandler(ThreadPoolExecutor.CallerRunsPolicy())
+            setWaitForTasksToCompleteOnShutdown(true)
+            setAwaitTerminationSeconds(20)
+            setAcceptTasksAfterContextClose(false)
+            setAllowCoreThreadTimeOut(false)
         }
     }
 
