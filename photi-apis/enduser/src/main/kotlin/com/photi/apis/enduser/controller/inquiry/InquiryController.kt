04a03b4eec37b3dd4f1003ee90e81dacc4d1a933
YuGyeong98 <kuk3097@naver.com>
2025-10-17 00:18:29 +0900

    #268 [refactor] inquiry 컨트롤러 수정

--- photi-apis/enduser/src/main/kotlin/com/photi/apis/enduser/controller/inquiry/InquiryController.kt
+++ photi-apis/enduser/src/main/kotlin/com/photi/apis/enduser/controller/inquiry/InquiryController.kt
@@ -3,13 +3,14 @@ package com.photi.apis.enduser.controller.inquiry
 import com.photi.apis.enduser.common.exception.annotation.GlobalApiErrorResponses
 import com.photi.apis.enduser.common.exception.annotation.UserApiErrorResponses
 import com.photi.apis.enduser.common.success.dto.StringSuccessResponse
+import com.photi.apis.enduser.config.security.AuthUser
+import com.photi.apis.enduser.config.security.CustomUserDetails
+import com.photi.apis.enduser.config.security.getUserId
 import com.photi.apis.enduser.controller.inquiry.dto.request.CreateInquiryRequest
 import com.photi.core.domain.common.consts.SwaggerKey.ACCESS_TOKEN_KEY
-import com.photi.core.domain.common.exception.GlobalErrorCode.TOKEN_UNAUTHENTICATED
-import com.photi.core.domain.common.exception.GlobalErrorCode.TOKEN_UNAUTHORIZED
+import com.photi.core.domain.common.exception.GlobalErrorCode.*
 import com.photi.core.domain.inquiry.service.InquiryService
 import com.photi.core.domain.user.exception.UserErrorCode.USER_NOT_FOUND
-import com.photi.utils.UserUtil
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.security.SecurityRequirement
@@ -22,11 +23,10 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import java.security.Principal
 
 @Validated
 @RestController
-@RequestMapping("/api/inquiries")
+@RequestMapping("/api/v2/inquiries")
 @Tag(name = "Inquiry", description = "문의 API")
 class InquiryController(
     private val inquiryService: InquiryService,
@@ -35,13 +35,13 @@ class InquiryController(
     @PostMapping
     @Operation(summary = "문의하기", security = [SecurityRequirement(name = ACCESS_TOKEN_KEY)])
     @ApiResponse(responseCode = "201")
-    @GlobalApiErrorResponses([TOKEN_UNAUTHENTICATED, TOKEN_UNAUTHORIZED])
+    @GlobalApiErrorResponses([TOKEN_UNAUTHENTICATED, EXPIRED_TOKEN, INVALID_TOKEN])
     @UserApiErrorResponses([USER_NOT_FOUND])
     fun createChallenge(
-        principal: Principal,
+        @AuthUser user: CustomUserDetails,
         @RequestBody @Valid request: CreateInquiryRequest,
     ): ResponseEntity<StringSuccessResponse> {
-        inquiryService.createInquiry(UserUtil.getUserId(principal), request.toServiceDto())
+        inquiryService.createInquiry(user.getUserId(), request.toServiceDto())
         return ResponseEntity.status(CREATED)
             .body(StringSuccessResponse("문의 접수가 완료되었습니다."))
     }
