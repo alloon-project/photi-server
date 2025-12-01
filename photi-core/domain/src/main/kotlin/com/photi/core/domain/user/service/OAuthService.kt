c0db85c68af3ad51c3defe82ff7bbaf20df702f6
YuGyeong98 <kuk3097@naver.com>
2025-12-01 17:00:26 +0900

    #278 [refactor] oauth 회원가입, 로그인 서비스 수정

--- photi-core/domain/src/main/kotlin/com/photi/core/domain/user/service/OAuthService.kt
+++ photi-core/domain/src/main/kotlin/com/photi/core/domain/user/service/OAuthService.kt
@@ -1,11 +1,12 @@
 package com.photi.core.domain.user.service
 
-import com.photi.core.domain.common.properties.KakaoOAuthProperties
 import com.photi.core.domain.user.dto.LoginDto
 import com.photi.core.domain.user.dto.OAuthSignUpDto
+import com.photi.core.domain.user.dto.OidcPayload
 import com.photi.core.domain.user.dto.SignUpDto
 import com.photi.core.domain.user.exception.UserException
-import com.photi.core.domain.user.model.OAuthInfo
+import com.photi.core.domain.user.model.OAuthProviderType
+import com.photi.core.domain.user.port.OAuthFactoryPort
 import com.photi.core.domain.user.port.OAuthPort
 import com.photi.core.domain.user.service.command.UserCommandService
 import com.photi.core.domain.user.service.query.UserQueryService
@@ -16,35 +17,39 @@ import org.springframework.transaction.annotation.Transactional
 @Service
 @Transactional(readOnly = true)
 class OAuthService(
-    private val oAuthPort: OAuthPort,
-    private val kakaoOAuthProperties: KakaoOAuthProperties,
+    private val oAuthFactory: OAuthFactoryPort,
     private val userCommandService: UserCommandService,
     private val userQueryService: UserQueryService,
     private val userValidator: UserValidator,
 ) {
 
     @Transactional
-    fun kakaoSignUp(idToken: String, dto: OAuthSignUpDto): SignUpDto {
-        val idTokenPayload = getOidcPayload(idToken)
-        val oAuthInfo = OAuthInfo.ofKakao(idTokenPayload.sub)
+    fun signUp(provider: OAuthProviderType, idToken: String, dto: OAuthSignUpDto): SignUpDto {
+        val oAuthPort = oAuthFactory.getOAuthAdapter(provider)
+        val idTokenPayload = getOidcPayload(idToken, oAuthPort)
+        val oAuthInfo = oAuthPort.createOAuthInfo(idTokenPayload.sub)
         if (userQueryService.existsBy(oAuthInfo)) throw UserException.ExistsUserException()
         val user = userCommandService.createUser(dto, oAuthInfo, idTokenPayload.email)
         return SignUpDto.of(user)
     }
 
-    fun kakaoLogin(idToken: String): LoginDto {
-        val idTokenPayload = getOidcPayload(idToken)
-        val oAuthInfo = OAuthInfo.ofKakao(idTokenPayload.sub)
+    fun login(provider: OAuthProviderType, idToken: String): LoginDto {
+        val oAuthPort = oAuthFactory.getOAuthAdapter(provider)
+        val idTokenPayload = getOidcPayload(idToken, oAuthPort)
+        val oAuthInfo = oAuthPort.createOAuthInfo(idTokenPayload.sub)
         val user = userQueryService.getLoginUserBy(oAuthInfo)
             ?: throw UserException.NotFoundUserException()
         user.login(userValidator)
         return LoginDto.of(user)
     }
 
-    private fun getOidcPayload(idToken: String) = oAuthPort.getIdTokenPayload(
-        idToken,
-        kakaoOAuthProperties.baseUrl,
-        kakaoOAuthProperties.restApiKey,
-        "nonce", // todo '인가 코드 요청 api' 요청 시 전달한 nonce 값과 동일한 값
-    )
+    private fun getOidcPayload(idToken: String, oAuthPort: OAuthPort): OidcPayload {
+        val properties = oAuthPort.getProperties()
+        return oAuthPort.getIdTokenPayload(
+            idToken,
+            properties.baseUrl,
+            properties.restApiKey,
+            "nonce", // todo '인가 코드 요청 api' 요청 시 전달한 nonce 값과 동일한 값
+        )
+    }
 }
