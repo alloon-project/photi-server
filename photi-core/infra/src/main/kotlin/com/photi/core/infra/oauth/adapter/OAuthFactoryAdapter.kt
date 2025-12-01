122914668d94ccdeb729c7a8e6c2ee073dea5388
YuGyeong98 <kuk3097@naver.com>
2025-12-01 16:58:51 +0900

    #278 [feat] oauth 팩토리 클래스 구현

---
+++ photi-core/infra/src/main/kotlin/com/photi/core/infra/oauth/adapter/OAuthFactoryAdapter.kt
@@ -0,0 +1,18 @@
+package com.photi.core.infra.oauth.adapter
+
+import com.photi.core.domain.user.model.OAuthProviderType
+import com.photi.core.domain.user.port.OAuthFactoryPort
+import org.springframework.stereotype.Component
+
+@Component
+class OAuthFactoryAdapter(
+    private val kakaoOAuthAdapter: KakaoOAuthAdapter,
+    private val googleOAuthAdapter: GoogleOAuthAdapter,
+) : OAuthFactoryPort {
+
+    override fun getOAuthAdapter(provider: OAuthProviderType) = when (provider) {
+        OAuthProviderType.KAKAO -> kakaoOAuthAdapter
+        OAuthProviderType.GOOGLE -> googleOAuthAdapter
+        OAuthProviderType.APPLE -> TODO()
+    }
+}
