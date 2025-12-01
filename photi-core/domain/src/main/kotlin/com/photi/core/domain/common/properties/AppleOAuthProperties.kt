cac38111e7dff816999938edd58ec19c032c48ec
YuGyeong98 <kuk3097@naver.com>
2025-12-01 23:42:10 +0900

    #279 [feat] 애플 configuration properties 추가

---
+++ photi-core/domain/src/main/kotlin/com/photi/core/domain/common/properties/AppleOAuthProperties.kt
@@ -0,0 +1,9 @@
+package com.photi.core.domain.common.properties
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+
+@ConfigurationProperties("oauth.apple")
+data class AppleOAuthProperties(
+    override val baseUrl: String,
+    override val restApiKey: String,
+) : OAuthProperties
--- photi-core/infra/src/main/kotlin/com/photi/core/infra/properties/ConfigurationPropertiesConfig.kt
+++ photi-core/infra/src/main/kotlin/com/photi/core/infra/properties/ConfigurationPropertiesConfig.kt
@@ -1,5 +1,6 @@
 package com.photi.core.infra.properties
 
+import com.photi.core.domain.common.properties.AppleOAuthProperties
 import com.photi.core.domain.common.properties.GoogleOAuthProperties
 import com.photi.core.domain.common.properties.KakaoOAuthProperties
 import com.photi.core.infra.PhotiConfig
@@ -7,5 +8,11 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-@EnableConfigurationProperties(value = [KakaoOAuthProperties::class, GoogleOAuthProperties::class])
+@EnableConfigurationProperties(
+    value = [
+        KakaoOAuthProperties::class,
+        GoogleOAuthProperties::class,
+        AppleOAuthProperties::class,
+    ]
+)
 class ConfigurationPropertiesConfig : PhotiConfig
