11fded16d80dd7d72ff3ba982acea60cdce408e0
YuGyeong98 <kuk3097@naver.com>
2025-12-22 00:38:10 +0900

    #279 [fix] json 역직렬화 오류 수정

--- photi-core/infra/src/main/kotlin/com/photi/core/infra/oauth/dto/OidcPublicKeysResponse.kt
+++ photi-core/infra/src/main/kotlin/com/photi/core/infra/oauth/dto/OidcPublicKeysResponse.kt
@@ -1,14 +1,14 @@
 package com.photi.core.infra.oauth.dto
 
 data class OidcPublicKeysResponse(
-    val keys: List<Jwk>,
+    val keys: List<Jwk> = listOf(),
 )
 
 data class Jwk(
-    val kid: String,
-    val kty: String,
-    val alg: String,
-    val use: String,
-    val n: String,
-    val e: String,
+    val kid: String = "",
+    val kty: String = "",
+    val alg: String = "",
+    val use: String = "",
+    val n: String = "",
+    val e: String = "",
 )
