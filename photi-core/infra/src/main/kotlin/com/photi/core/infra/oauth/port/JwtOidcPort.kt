9de4e2ea206704d0b468f846ab68bd6cb30c1426
YuGyeong98 <kuk3097@naver.com>
2026-01-01 00:42:39 +0900

    #286 [feat] idtoken aud 추출 함수 구현

--- photi-core/infra/src/main/kotlin/com/photi/core/infra/oauth/port/JwtOidcPort.kt
+++ photi-core/infra/src/main/kotlin/com/photi/core/infra/oauth/port/JwtOidcPort.kt
@@ -4,6 +4,8 @@ import com.photi.core.domain.user.dto.OidcPayload
 
 interface JwtOidcPort {
 
+    fun getAudFromUnsignedIdToken(idToken: String): String
+
     fun getKidFromUnsignedIdToken(idToken: String, iss: String, aud: String): String
 
     fun getIdTokenPayload(idToken: String, modulus: String, exponent: String): OidcPayload
