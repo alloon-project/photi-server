4ec29a645a98f259907ed9969b6ec5f66e470d80
YuGyeong98 <kuk3097@naver.com>
2026-04-22 19:54:52 +0900

    #301 [refactor] 브릿지 페이지 수정

--- photi-apis/enduser/src/main/kotlin/com/photi/apis/enduser/controller/bridge/BridgeViewController.kt
+++ photi-apis/enduser/src/main/kotlin/com/photi/apis/enduser/controller/bridge/BridgeViewController.kt
@@ -2,7 +2,6 @@ package com.photi.apis.enduser.controller.bridge
 
 import io.swagger.v3.oas.annotations.Parameter
 import org.springframework.stereotype.Controller
-import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 
@@ -11,10 +10,8 @@ class BridgeViewController {
 
     @GetMapping("/open")
     fun openApp(
-        model: Model,
-        @RequestParam("challenge_id") @Parameter(description = "챌린지 id") challengeId: Long,
+        @RequestParam("challenge_id") @Parameter(description = "챌린지 id") challengeId: Long
     ): String {
-        model.addAttribute("challengeId", challengeId)
         return "bridge"
     }
 }
