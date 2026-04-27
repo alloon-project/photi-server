69c29c4c34090cb819e29e8614ed7faea6e904aa
YuGyeong98 <kuk3097@naver.com>
2026-04-28 07:02:57 +0900

    [fix] 필요없는 부분 삭제

--- photi-core/domain/src/main/kotlin/com/photi/core/domain/common/properties/AppleOAuthProperties.kt
+++ photi-core/domain/src/main/kotlin/com/photi/core/domain/common/properties/AppleOAuthProperties.kt
@@ -1,10 +1,8 @@
 package com.photi.core.domain.common.properties
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.boot.context.properties.ConfigurationProperties
 
 @ConfigurationProperties("oauth.apple")
-@ConditionalOnProperty(prefix = "oauth.apple", name = ["base-url"])
 data class AppleOAuthProperties(
     val baseUrl: String,
     val restApiKey: String,
