0bf200742c2a52e6c083716825f02866f221ae60
YuGyeong98 <kuk3097@naver.com>
2026-04-29 22:29:46 +0900

    [fix] batch 모듈 오류 해결

--- photi-core/infra/src/main/kotlin/com/photi/core/infra/oauth/adapter/OAuthFactoryAdapter.kt
+++ photi-core/infra/src/main/kotlin/com/photi/core/infra/oauth/adapter/OAuthFactoryAdapter.kt
@@ -2,9 +2,12 @@ package com.photi.core.infra.oauth.adapter
 
 import com.photi.core.domain.user.model.OAuthProviderType
 import com.photi.core.domain.user.port.OAuthFactoryPort
+import com.photi.core.infra.oauth.port.JwtOidcPort
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean
 import org.springframework.stereotype.Component
 
 @Component
+@ConditionalOnBean(JwtOidcPort::class)
 class OAuthFactoryAdapter(
     private val kakaoOAuthAdapter: KakaoOAuthAdapter,
     private val googleOAuthAdapter: GoogleOAuthAdapter,
